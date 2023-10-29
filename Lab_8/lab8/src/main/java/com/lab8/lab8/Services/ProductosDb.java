b77c6ae1c3d148dad38b62587da09c5d0df3ea8d
Waldoox <kevin.barria1@utp.ac.pa>
2023-10-28 23:39:27 -0500

    Lab 8

--- Lab_8/lab8/src/main/java/com/lab8/lab8/Services/ProductosDb.java
+++ Lab_8/lab8/src/main/java/com/lab8/lab8/Services/ProductosDb.java
@@ -5,39 +5,101 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
 import com.lab8.lab8.Models.Productos;
 
 public class ProductosDb {
-    Connection _cn;
+  Connection _cn;
 
-    public ProductosDb(){
-        _cn = new Conexion().openDb();
+  public ProductosDb() {
+    _cn = new Conexion().openDb();
+  }
+
+  public List<Productos> ObtenerProductos() {
+    try {
+      Statement stmt = _cn.createStatement();
+      String query = "SELECT * FROM productos";
+
+      List<Productos> productos = new ArrayList<>();
+      ResultSet result = stmt.executeQuery(query);
+      while (result.next()) {
+        Productos producto = new Productos(
+            result.getInt("id"),
+            result.getString("nombre"),
+            result.getFloat("precio"),
+            result.getInt("categoria_id"),
+            result.getString("fotoUrl"),
+            result.getString("fecha_expiracion"),
+            result.getString("fecha_preparacion"));
+
+        productos.add(producto);
+      }
+
+      result.close();
+      stmt.close();
+      return productos;
+
+    } catch (Exception e) {
+      int x = 1;
     }
+    return null;
+  }
 
-    public List<Productos> ObtenerProductos(){
-        try {
-            Statement stmt = _cn.createStatement();
-            String query = "SELECT * FROM productos";
+  public int GuardarProductos(Productos producto) {
+    int resultado = 0;
+    try {
+      Statement stm = _cn.createStatement();
+      String query = "Call InsertarProducto('"
+          + producto.getNombre() + "',"
+          + producto.getPrecio() + ","
+          + producto.getCategoriaId() + ",'"
+          + producto.getFoto() + "','"
+          + producto.getFechaProduccion() + "','"
+          + producto.getFechaCaducidad() + "')";
 
-            List<Productos> productos = new ArrayList<>();
-            ResultSet result = stmt.executeQuery(query);
-            while (result.next()) {
-                Productos producto = new Productos(
-                    result.getString("nombre"),
-                    result.getFloat("precio"),
-                    result.getString("foto")
-                );
-                productos.add(producto);
-            }
-            result.close();
-            stmt.close();
-            return productos;
+      resultado = stm.executeUpdate(query);
 
-        } catch (Exception e) {
-            int x = 1;
-        }
-
-        return null;
-
+      return resultado;
+    } catch (Exception e) {
+      int x = 1;
     }
+    return resultado;
+  }
+
+  public int ActualizarProductos(Productos producto) {
+    int resultado = 0;
+    try {
+      Statement stm = _cn.createStatement();
+      String query = "Call ActualizarProducto("
+          + producto.getId() + ",'"
+          + producto.getNombre() + "',"
+          + producto.getPrecio() + ","
+          + producto.getCategoriaId() + ",'"
+          + producto.getFoto() + "','"
+          + producto.getFechaProduccion() + "','"
+          + producto.getFechaCaducidad() + "')";
+
+      resultado = stm.executeUpdate(query);
+
+      return resultado;
+    } catch (Exception e) {
+      int x = 1;
+    }
+    return resultado;
+  }
+
+  public int EliminarProducto(int pid) {
+    int resultado = 0;
+    try {
+      Statement stmt = _cn.createStatement();
+      String query = "Call EliminarProducto(" + pid + ")";
+
+      return stmt.executeUpdate(query);
+
+    } catch (Exception e) {
+      int x = 1;
+    }
+    return resultado;
+  }
+
 }
